31a738bf9e1284773d3dc4993838e80a01f04526
koya <koya9431ptr@gmail.com>
2025-05-12 11:39:08 -0700

    install shadcn ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
     </>
   );
 }
