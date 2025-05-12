1957ddbc30412435921eba4e549134045c538d00
gustavofantin <gustavofantinbarros@gmail.com>
2025-05-12 12:05:17 -0700

    backend folder set up

---
+++ backend/src/server.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import mongoose from 'mongoose';
+dotenv.config();
+
+
+//Create server
+const app = express();
+
+//Middlewares
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true, // allow cookies
+  })
+);
+app.use(express.json());
+
+
+//Fallback
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ error: '404 Not Found', message: 'Page not found.' });
+});
+
+
+//Connect to MongoDb
+const MONGODB_URI = process.env.DATABASE_URI!;
+mongoose
+  .connect(MONGODB_URI, { dbName: 'daisuki' })
+  .then(() => {
+    console.log('Connected to MongoDB database');
+
+    //Start server
+    const PORT = process.env.PORT || 3500;
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+  });
